7561318ebbf7e6b9ec754a10e34f89f18a82a5d1
Matheus <oliveiramatheus21322@gmail.com>
2025-03-20 14:58:23 -0300

    Add files via upload

---
+++ src/pages/home.jsx
@@ -0,0 +1,43 @@
+import { useTheme } from "../context/ThemeContext";
+import { Link } from "react-router";
+import { Github, Instagram, Youtube, Linkedin, Moon, Sun } from "lucide-react";
+import "./style.css";
+import foto from "./img/cabeludo.png";
+
+export function Home() {
+  const { toggleTheme, theme } = useTheme();
+
+  return (
+    <>
+      <div className="Card">
+        <img className="foto" src={foto} alt="Zacarias" />
+        <p>@matheus</p>
+
+        <div className="Buttons">
+          <div className="toggle-container" onClick={toggleTheme}>
+            <div
+              className={`toggle-switch ${theme === "dark" ? "dark" : "light"}`}
+            >
+              {theme === "dark" ? (
+                <Moon size={20} color="black" />
+              ) : (
+                <Sun size={20} color="black" />
+              )}
+            </div>
+          </div>
+          <button>Inscreva-se no NLW</button>
+          <button>Baixe meu E-BOOK</button>
+          <button>Veja meu portifólio</button>
+          <button>Conheça o Exploer</button>
+        </div>
+
+        <div className="Social">
+          <Github />
+          <Instagram />
+          <Youtube />
+          <Linkedin />
+        </div>
+      </div>
+    </>
+  );
+}
